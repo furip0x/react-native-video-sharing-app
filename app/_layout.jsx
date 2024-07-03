ac5da4dba7374dc3f9bd2ac0112e6a7d1b441bb2
furip0x <furkanipekci@hotmail.com>
2024-07-03 08:38:39 +0300

    Change font names

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -7,13 +7,15 @@ SplashScreen.preventAutoHideAsync()
 
 const RootLayout = () => {
   const [fontsLoaded, error] = useFonts({
-    PoppinsBlack: require("../assets/fonts/PoppinsBlack.ttf"),
-    PoppinsBold: require("../assets/fonts/PoppinsBold.ttf"),
-    PoppinsExtraBold: require("../assets/fonts/PoppinsExtraBold.ttf"),
-    PoppinsLight: require("../assets/fonts/PoppinsLight.ttf"),
-    PoppinsMedium: require("../assets/fonts/PoppinsMedium.ttf"),
-    PoppinsRegular: require("../assets/fonts/PoppinsRegular.ttf"),
-    PoppinsThin: require("../assets/fonts/PoppinsThin.ttf"),
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
   })
 
   useEffect(() => {
